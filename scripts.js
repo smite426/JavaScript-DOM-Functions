309661e30e594ceb342edae05318d887eae8f08b
Elliott Smith <smite426@hsd.k12.or.us>
2022-05-11 10:23:20 -0700

    added mouse event

--- scripts.js
+++ scripts.js
@@ -3,3 +3,15 @@ var par = document.createElement('P');
 par.textContent = 'The DOM is the bomb!';
 
 document.body.appendChild(par);
+
+var div = document.createElement('DIV');
+
+div.style.height = '100vh';
+
+document.body.appendChild(div);
+
+div.appendChild(par)
+
+div.addEventListener('mousemove', function(event) {
+    console.log(event)
+});
